6ee837c29e830f1e32ce88779cbded412a21c061
Angel Juarez <angeljuarez729@gmail.com>
2020-07-27 19:00:18 -0400

    exercise

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,23 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    {
+      name: "Burger",
+      description: "This is definitely an American food. Which means it's pretty good",
+      image: "https://media1.s-nbcnews.com/j/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p_d9270c5c545b30ea094084c7f2342eb4.fit-760w.jpg"
+    },
+    {
+      name: "Hot Dogs",
+      description: "Hot dogs are nice and they are very convenient to eat.",
+      image: "https://potatorolls.com/wp-content/uploads/Basic-Hot-Dogs.jpg"
+    },
+    {
+      name: "Fettuccine Alfredo with Chicken",
+      description: "One time Angel tried to cook this dish from scratch and he ended up overdoing it with the cheese because he didn't realize the specific cheese he was using wasn't supposed to melt.",
+      image: "https://www.jessicagavin.com/wp-content/uploads/2019/08/chicken-alfredo-8-1200.jpg"
+    }
+  ];
 
   trialRecipe: Recipe = {
     name: "tacos",
